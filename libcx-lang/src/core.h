c545f3fee03ca5836971a21367057826d9ecbf04
Luiz Fratoni <luizfratoni@gmail.com>
2019-07-09 12:16:40 -0300

    Core

--- libcx-lang/src/core.h
+++ libcx-lang/src/core.h
@@ -16,7 +16,21 @@ void CxError(const char*, ...);
 void CxLogPush(const char*);
 void CxLogPop();
 
-void Cx
+void CxStr_Init(const char* );
+
+unsigned int CxStr_Len(const char* );
+unsigned int CxStr_Equals(const char*, const char*, CxBool ignoreCase );
+unsigned int CxStr_Copy(const char*, const char* );
+unsigned int CxStr_CopySize(const char*, const char*, unsigned size);
+unsigned int CxStr_IndexOf(const char* , const char*);
+unsigned int CxStr_LastIndexOf(const char* , const char*);
+
+CxObj    CxStr_Split(const char*, const char*);
+
+CxObj    CxString(const char* );
+CxObj    CxInt(int v);
+CxObj    CxFloat(float f);
+CxObj    CxDouble(double );
 
 
 #ifdef __cplusplus
